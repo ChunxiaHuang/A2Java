90ebde470b8b61711be9644940b80fe7b8621a70
Chunxia Huang <chunxia.huang@edu.sait.ca>
2024-06-21 09:39:08 -0600

    Initial commit

---
+++ src/submission1/StackADT.java
@@ -0,0 +1,59 @@
+package submission1;
+
+/**
+ * This interface displays the abstract method for stack
+ * @param <E> the type of items in the stack
+ */
+public interface StackADT<E> {
+
+	/**
+	 * Add an item on the top of the stack
+	 * pre-conditions: the item is not null
+	 * post-conditions: the size of the stack will be added by 1
+	 * @param item  the item to be added to the top of the stack
+	 */
+	void push(E item);
+	
+	/**
+	 * 
+	 * 
+	 * 
+	 * Remove the top item of the stack
+	 * pre-condition: the stack is not empty
+	 * post-condition: the size will be reduced by 1
+	 * @return the item being removed
+	 */
+	E pop();
+	
+	/**
+	 * Look at the item on the top of the stack
+	 * pre-condition: the stack is not empty
+	 * post-condition: None
+	 * @return the item on the top of the stack
+	 */
+	E peek();
+	
+	/**
+	 * Return the number of items in the stack
+	 * pre-condition: None
+	 * post-condition: None
+	 * @return the number of the items in the stack
+	 */
+	int size();
+	
+	/**
+	 * Check if the stack is empty
+	 * pre-condition: None
+	 * post-condition: None
+	 * @return return true if the stack is empty, return false if not
+	 */
+	boolean isEmpty();
+	
+	/**
+	 * Remove all items in the stack
+	 * pre-condition: None
+	 * post-condition: the size of the stack will be 0
+	 */
+	void clear();
+	
+}

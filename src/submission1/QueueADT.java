ea85b058f5ddc9f0e97c63e7c943080b452bff42
Chunxia Huang <chunxia.huang@edu.sait.ca>
2024-06-27 14:15:01 -0600

    update to interface

--- src/submission1/QueueADT.java
+++ src/submission1/QueueADT.java
@@ -3,127 +3,98 @@ package submission1;
 import java.io.Serializable;
 import java.util.Iterator;
 
-public class QueueADT<E> implements Serializable {
+public interface QueueADT<E> extends Serializable {
 
+	 /**
+     * Add an item to the end of the queue
+     * pre-condition: the item is not null
+     * post-condition: the item will be added to the end of the queue
+     * @param toAdd the item to be added
+     * @throws NullPointerException when the item to add is null
+     */
+    public void enqueue(E toAdd);
 
-    public void enqueue(E toAdd) {
-        /**
-         * Add an item to the end of the queue
-         * pre-condition: the item is not null
-         * post-condition: the item will be added to the end of the queue
-         * @param toAdd the item to be added
-         * @throws NullPointerException when the item to add is null
-         */
-    }
+    /**
+     * Remove the front item of the queue
+     * pre-condition: the queue is not empty
+     * post-condition: the size will be reduced by 1
+     * @return the item being removed
+     * @throws EmptyQueueException when the queue is empty
+     */
+    public E dequeue();
 
-    public E dequeue() {
-	    /**
-	     * Remove the front item of the queue
-	     * pre-condition: the queue is not empty
-	     * post-condition: the size will be reduced by 1
-	     * @return the item being removed
-	     * @throws EmptyQueueException when the queue is empty
-	     */
-        return null;
-    }
-
+    /**
+     * Look at the front item of the queue without removing it
+     * pre-condition: the queue is not empty
+     * post-condition: the queue remains unchanged
+     * @return the item at the front of the queue
+     * @throws EmptyQueueException when the queue is empty
+     */
+    public E peek();
   
-    public E peek() {
-    	 /**
-         * Look at the front item of the queue without removing it
-         * pre-condition: the queue is not empty
-         * post-condition: the queue remains unchanged
-         * @return the item at the front of the queue
-         * @throws EmptyQueueException when the queue is empty
-         */
-        return null;
-    }
-  
-    public void dequeueAll() {
-    	  /**
-         * Remove all items from the queue
-         * pre-condition: none
-         * post-condition: the queue will be empty
-         */
-    }
+    /**
+     * Remove all items from the queue
+     * pre-condition: none
+     * post-condition: the queue will be empty
+     */
+    public void dequeueAll();
 
+    /**
+     * Check if the queue is empty
+     * pre-condition: none
+     * post-condition: none
+     * @return true if the queue is empty, false otherwise
+     */
+    public boolean isEmpty();
 
-    public boolean isEmpty() {
-        /**
-         * Check if the queue is empty
-         * pre-condition: none
-         * post-condition: none
-         * @return true if the queue is empty, false otherwise
-         */
-        return false;
-    }
+    /**
+     * Get an iterator for the queue
+     * pre-condition: none
+     * post-condition: none
+     * @return an iterator for the queue
+     */
+    public Iterator<E> iterator();
 
+    /**
+     * Compare this queue with another queue
+     * pre-condition: none
+     * post-condition: none
+     * @param that the queue to compare with
+     * @return true if the queues are equal, false otherwise
+     */
+    public boolean equals(QueueADT<E> that);
 
-    public Iterator<E> iterator() {
-        /**
-         * Get an iterator for the queue
-         * pre-condition: none
-         * post-condition: none
-         * @return an iterator for the queue
-         */
-        return null;
-    }
+    /**
+     * Convert the queue to an array
+     * pre-condition: none
+     * post-condition: none
+     * @return an array containing all items in the queue
+     */
+    public Object[] toArray();
 
+    /**
+     * Convert the queue to an array of a specific type
+     * pre-condition: the provided array is not null
+     * post-condition: none
+     * @param holder the array to hold the queue items
+     * @return an array containing all items in the queue
+     * @throws NullPointerException when the provided array is null
+     */
+    public E[] toArray(E[] holder);
 
-    public boolean equals(QueueADT<E> that) {
-        /**
-         * Compare this queue with another queue
-         * pre-condition: none
-         * post-condition: none
-         * @param that the queue to compare with
-         * @return true if the queues are equal, false otherwise
-         */
-        return false;
-    }
+    /**
+     * Check if the queue is full (optional method)
+     * pre-condition: none
+     * post-condition: none
+     * @return true if the queue is at capacity, false otherwise
+     */
+    public boolean isFull();
 
-
-    public Object[] toArray() {
-        /**
-         * Convert the queue to an array
-         * pre-condition: none
-         * post-condition: none
-         * @return an array containing all items in the queue
-         */
-        return null;
-    }
-
-
-    public E[] toArray(E[] holder) {
-        /**
-         * Convert the queue to an array of a specific type
-         * pre-condition: the provided array is not null
-         * post-condition: none
-         * @param holder the array to hold the queue items
-         * @return an array containing all items in the queue
-         * @throws NullPointerException when the provided array is null
-         */
-        return null;
-    }
-
-
-    public boolean isFull() {
-        /**
-         * Check if the queue is full (optional method)
-         * pre-condition: none
-         * post-condition: none
-         * @return true if the queue is at capacity, false otherwise
-         */
-        return false;
-    }
-
-
-    public int size() {
-        /**
-         * Get the size of the queue
-         * pre-condition: none
-         * post-condition: none
-         * @return the number of items in the queue
-         */
-        return 0;
-    }
+    /**
+     * Get the size of the queue
+     * pre-condition: none
+     * post-condition: none
+     * @return the number of items in the queue
+     */
+    public int size();
 }
--- src/submission1/StackADT.java
+++ src/submission1/StackADT.java
@@ -4,131 +4,110 @@ import java.io.Serializable;
 import java.util.EmptyStackException;
 import java.util.Iterator;
 
-public class StackADT<E> implements Serializable {
+public interface StackADT<E> extends Serializable {
 
-    public void push(E toAdd) {
-        /**
-         * Add an item to the top of the stack
-         * pre-condition: the item is not null
-         * post-condition: the item will be added to the top of the stack
-         * @param toAdd the item to be added
-         * @throws NullPointerException when the item to add is null
-         */
-    }
+	 /**
+     * Add an item to the top of the stack
+     * pre-condition: the item is not null
+     * post-condition: the item will be added to the top of the stack
+     * @param toAdd the item to be added
+     * @throws NullPointerException when the item to add is null
+     */
+    public void push(E toAdd);
 
-    public E pop() {
-        /**
-         * Remove the top item of the stack
-         * pre-condition: the stack is not empty
-         * post-condition: the size will be reduced by 1
-         * @return the item being removed
-         * @throws EmptyStackException when the stack is empty
-         */
-        return null;
-    }
+    /**
+     * Remove the top item of the stack
+     * pre-condition: the stack is not empty
+     * post-condition: the size will be reduced by 1
+     * @return the item being removed
+     * @throws EmptyStackException when the stack is empty
+     */
+    public E pop();
+    
+    /**
+     * Look at the top item of the stack without removing it
+     * pre-condition: the stack is not empty
+     * post-condition: the stack remains unchanged
+     * @return the item at the top of the stack
+     * @throws EmptyStackException when the stack is empty
+     */
+    public E peek();
 
-    public E peek() {
-        /**
-         * Look at the top item of the stack without removing it
-         * pre-condition: the stack is not empty
-         * post-condition: the stack remains unchanged
-         * @return the item at the top of the stack
-         * @throws EmptyStackException when the stack is empty
-         */
-        return null;
-    }
+    /**
+     * Remove all items from the stack
+     * pre-condition: none
+     * post-condition: the stack will be empty
+     */
+    public void clear();
+    
+    /**
+     * Check if the stack is empty
+     * pre-condition: none
+     * post-condition: none
+     * @return true if the stack is empty, false
+     */
+    public boolean isEmpty();
 
-    public void clear() {
-        /**
-         * Remove all items from the stack
-         * pre-condition: none
-         * post-condition: the stack will be empty
-         */
-    }
+    /**
+     * Convert the stack to an array
+     * pre-condition: none
+     * post-condition: none
+     * @return an array containing all items in the stack
+     */
+    public Object[] toArray();
 
-    public boolean isEmpty() {
-        /**
-         * Check if the stack is empty
-         * pre-condition: none
-         * post-condition: none
-         * @return true if the stack is empty, false
-         */
-        return false;
-    }
+    /**
+     * Convert the stack to an array of a specific type
+     * pre-condition: the provided array is not null
+     * post-condition: none
+     * @param holder the array to hold the stack items
+     * @return an array containing all items in the stack
+     * @throws NullPointerException when the provided array is null
+     */
+    public E[] toArray(E[] holder);
 
-    public Object[] toArray() {
-        /**
-         * Convert the stack to an array
-         * pre-condition: none
-         * post-condition: none
-         * @return an array containing all items in the stack
-         */
-        return null;
-    }
+    /**
+     * Check if the stack contains a specific item
+     * pre-condition: the item is not null
+     * post-condition: none
+     * @param toFind the item to find
+     * @return true if the stack contains the item, false otherwise
+     * @throws NullPointerException when the item to find is null
+     */
+    public boolean contains(E toFind);
 
-    public E[] toArray(E[] holder) {
-        /**
-         * Convert the stack to an array of a specific type
-         * pre-condition: the provided array is not null
-         * post-condition: none
-         * @param holder the array to hold the stack items
-         * @return an array containing all items in the stack
-         * @throws NullPointerException when the provided array is null
-         */
-        return null;
-    }
 
-    public boolean contains(E toFind) {
-        /**
-         * Check if the stack contains a specific item
-         * pre-condition: the item is not null
-         * post-condition: none
-         * @param toFind the item to find
-         * @return true if the stack contains the item, false otherwise
-         * @throws NullPointerException when the item to find is null
-         */
-        return false;
-    }
+    /**
+     * Find the position of an item in the stack
+     * pre-condition: none
+     * post-condition: none
+     * @param toFind the item to find
+     * @return the 1-based position of the item in the stack, or -1 if not found
+     */
+    public int search(E toFind);
 
-    public int search(E toFind) {
-        /**
-         * Find the position of an item in the stack
-         * pre-condition: none
-         * post-condition: none
-         * @param toFind the item to find
-         * @return the 1-based position of the item in the stack, or -1 if not found
-         */
-        return 0;
-    }
+    /**
+     * Get an iterator for the stack
+     * pre-condition: none
+     * post-condition: none
+     * @return an iterator for the stack
+     */
+    public Iterator<E> iterator();
 
-    public Iterator<E> iterator() {
-        /**
-         * Get an iterator for the stack
-         * pre-condition: none
-         * post-condition: none
-         * @return an iterator for the stack
-         */
-        return null;
-    }
+    /**
+     * Compare this stack with another stack
+     * pre-condition: none
+     * post-condition: none
+     * @param that the stack to compare with
+     * @return true if the stacks are equal, false otherwise
+     */
+    public boolean equals(StackADT<E> that);
 
-    public boolean equals(StackADT<E> that) {
-        /**
-         * Compare this stack with another stack
-         * pre-condition: none
-         * post-condition: none
-         * @param that the stack to compare with
-         * @return true if the stacks are equal, false otherwise
-         */
-        return false;
-    }
-
-    public int size() {
-        /**
-         * Get the size of the stack
-         * pre-condition: none
-         * post-condition: none
-         * @return the number of items in the stack
-         */
-        return 0;
-    }
+    /**
+     * Get the size of the stack
+     * pre-condition: none
+     * post-condition: none
+     * @return the number of items in the stack
+     */
+    public int size();
 }
